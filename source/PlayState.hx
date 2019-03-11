b15f5ef50c7c1388dac5874f89e9323f9b0eca0e
Richard Bray <richardbray89@gmail.com>
2019-03-11 14:15:12 +0000

    start adding basic level

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,11 +2,15 @@ package;
 
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.tiled.TiledMap;
 
 // import flixel.util.FlxColor;
 class PlayState extends FlxState {
 	var _txtTitle:FlxText;
 	var _player:Player;
+	var _level:TiledMap;
+	var _ground:FlxTilemap;
 
 	override public function create():Void {
 		bgColor = 0xffc7e4db; // Game background color
@@ -16,8 +20,14 @@ class PlayState extends FlxState {
 		_txtTitle.screenCenter();
 		add(_txtTitle);
 
+		// add envirionment
+		_level = new TiledMap(AssetPaths.room_001__tmx);
+		// ideally load from CSV
+		// loadMapFromCSV("assets/levels/mapCSV_Group1_Map1back.csv", "assets/art/area02_level_tiles2.png", 16, 16));
+		_ground = new FlxTilemap();
+
 		// Add Player
-		_player = new Player(10,10);
+		_player = new Player(10, 10);
 		add(_player);
 		super.create();
 	}
