fb640c8b7dbb2875f60374580160090c2b8e3421
Richard Bray <richardbray89@gmail.com>
2019-05-01 15:14:46 +0100

    pause menu controls

--- source/PauseMenu.hx
+++ source/PauseMenu.hx
@@ -17,28 +17,27 @@ class PauseMenu extends FlxSubState {
 	var _pointer:FlxSprite;
 	var _choices:Array<FlxText>;
 	var _titleText:String;
-	var _playerDied:Bool;
 
 	public var gamePaused:Bool = false;
 
 	public function new(PlayerDied:Bool = false):Void {
 		super();
-		_playerDied = PlayerDied;
+		var _boxXPos:Float = (FlxG.width / 2) - (_menuWidth / 2);
 		_grpMenuItems = new FlxSpriteGroup();
 		_gameOverlay = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0x9c000000);
 		_grpMenuItems.add(_gameOverlay);
 
-		_boundingBox = new FlxSprite((FlxG.width / 2) - (_menuWidth / 2), (FlxG.height / 2) - (_menuHeight / 2));
+		_boundingBox = new FlxSprite(_boxXPos, (FlxG.height / 2) - (_menuHeight / 2));
 		_boundingBox.makeGraphic(_menuWidth, _menuHeight, 0xff205ab7);
 		_grpMenuItems.add(_boundingBox);
 
-		_titleText = _playerDied ? "GAME OVER" : "GAME PAUSED";
+		_titleText = PlayerDied ? "GAME OVER" : "GAME PAUSED";
 		_menuTitle = new FlxText(20, 110, 0, _titleText, 30);
 		_menuTitle.alignment = CENTER;
 		_menuTitle.screenCenter(X);
 		_grpMenuItems.add(_menuTitle);
 
-		_pointer = new FlxSprite(_menuTitle.x, _menuTitle.y + 200);
+		_pointer = new FlxSprite(_boxXPos, _menuTitle.y + 200);
 		_pointer.makeGraphic(_menuWidth, 60, 0xffdc2de4);
 		_grpMenuItems.add(_pointer);
 
@@ -82,6 +81,20 @@ class PauseMenu extends FlxSubState {
 					js.Browser.console.log('Quit game');
 				default:
 					js.Browser.console.log('Quit game');
+			}
+		}
+
+		if (FlxG.keys.anyJustPressed([DOWN, S])) {
+			if (_selected != _choices.length - 1) {
+				_pointer.y = _pointer.y + 60;
+				_selected++;
+			}
+		}
+
+		if (FlxG.keys.anyJustPressed([UP, W])) {
+			if (_selected != 0) {
+				_pointer.y = _pointer.y - 60;
+				_selected--;
 			}
 		}
 
