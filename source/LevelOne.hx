1be7d76ce69b86e05906b8315d5e2b08ccd6ea31
Richard Bray <richardbray89@gmail.com>
2019-08-15 14:04:28 +0100

    Makes reusable intro class

--- source/LevelOne.hx
+++ source/LevelOne.hx
@@ -129,75 +129,24 @@ class LevelOne extends LevelState {
 	}		
 }
 
-class Intro extends GameState {
-	var _facts:Array<String>;
-	var _factText:FlxText;
-	var _factNumber:Int = 0;
-	var _gameSave:FlxSave;
-	var _seconds:Float = 0;
-	var _controls:Controls;
-	var _textWidth:Int = 600;
+class Intro extends IntroState {
 
 	/**
 	 * Runs the intro sequence for the first level.
 	 *
-	 * @param GameSave	Game save from `MainMenu.hx`
+	 * @param GameSave Game save from `MainMenu.hx`
 	 */
 	public function new(GameSave:FlxSave) {
 		super();
 		_gameSave = GameSave;
-	}
-
-	override public function create() {
-		bgColor = 0xff04090C; // 04090C
-
-		// @todo, this will be passed into the Intro Class as a variable in the future
-		_facts = [
+		facts = [
 			"Pangolins are the most trafficked mammal in the world, between 2011 and 2013 around 117 million of them were killed.",
 			"They're in high demand from places like China and Vietnam for their meat and scales.",
 			"They love to eat bugs and are often called, 'the scaly anteater'. We join our hero doing – just that..."
-		];
-
-		_factText = new FlxText(
-			(FlxG.width / 2) - (_textWidth / 2), 
-			(FlxG.height / 2) - 100, 
-			_textWidth, 
-			_facts[_factNumber]
-		);
-		_factText.setFormat(Constants.squareFont, Constants.lrgFont);
-
-		FlxG.cameras.fade(FlxColor.BLACK, 0.5, true); // Level fades in
-		add(_factText);	
-		_factText.alpha = 0;
-		_controls = new Controls();		
-	}	
-
-	override public function update(Elapsed:Float) {
-		super.update(Elapsed);
-		_seconds += Elapsed;	
-	
-		// Starts level when all the facts have been looped through
-		(_factNumber == _facts.length) ? startLevel() : showFacts();
-
-		// Start level if player presses start
-		if ( _controls.start.check()) startLevel();
+		];		
 	}
 
-	function showFacts() {
-		_factText.text = _facts[_factNumber];
-		var showFor:Int = 4; // How many seconds the text should show for
-	 
-		if (_seconds < showFor) {
-			FlxTween.tween(_factText, { alpha: 1 }, .5);
-		} else if (_seconds > (showFor + 1) && _seconds < (showFor + 2)) {
-			FlxTween.tween(_factText, { alpha: 0 }, .5);
-		} else if (Math.round(_seconds) == (showFor + 3)) {
-			_seconds = 0;
-			_factNumber++;
-		}				
-	}
-
-	function startLevel() {
+	override public function startLevel() {
 		FlxG.switchState(new LevelOne(0, 3, null, _gameSave, true));
 	}
 }
