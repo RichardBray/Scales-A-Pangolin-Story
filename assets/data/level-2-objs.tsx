1649eb87202172f60f84982f224f1d23970f7a9e
Richard Bray <richardbray89@gmail.com>
2019-08-31 11:16:53 +0100

    Add snake to level 2

---
+++ assets/data/level-2-objs.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="level-2-objs" tilewidth="238" tileheight="120" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="238" height="120" source="../../../Dropbox/pangolin/snake-enemy.png"/>
+ </tile>
+</tileset>
