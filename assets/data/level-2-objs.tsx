10e8f6e47e90c0431c317bc2e2c9ce670565b72f
Richard Bray <richardbray89@gmail.com>
2019-09-25 08:36:18 +0100

    Apply more tweaks to level 4

--- assets/data/level-2-objs.tsx
+++ assets/data/level-2-objs.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="level-2-objs" tilewidth="238" tileheight="120" tilecount="1" columns="0">
+<tileset version="1.2" tiledversion="1.2.4" name="level-2-objs" tilewidth="338" tileheight="170" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="238" height="120" source="../../../Dropbox/pangolin/snake-enemy.png"/>
+ </tile>
+ <tile id="1">
+  <image width="338" height="170" source="../../../Dropbox/pangolin/leopard.png"/>
  </tile>
 </tileset>
