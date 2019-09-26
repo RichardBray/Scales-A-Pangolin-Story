a16cc323cf4292e00daf6e927a6eaa00f1e7a22a
Richard Bray <richardbray89@gmail.com>
2019-09-26 15:11:33 +0100

    Add leopard dying animation

--- source/Leopard.hx
+++ source/Leopard.hx
@@ -6,20 +6,21 @@ import flixel.FlxG;
 
 class Leopard extends Enemy {
   var _seconds:Float = 0;
+  var _enemyDying:Bool = false;
 
   public function new(X:Float, Y:Float) {
-    super(X, Y);
+    super(X, Y + 55);
     health = 4;
 
     loadGraphic("assets/images/leopard.png", true, 338, 170);
-
+    updateSpriteHitbox(78, 55, this);
 		setFacingFlip(FlxObject.LEFT, true, false);
 		setFacingFlip(FlxObject.RIGHT, false, false);  
 
 		// Animations
-		animation.add("walking", [for (i in 0...5) i], 5, true);
+		animation.add("walking", [for (i in 0...5) i], 6, true);
     animation.add("running", [for (i in 6...11) i], 12, true); 
-    animation.add("dying", [for (i in 12...17) i], 12, true);      
+    animation.add("dying", [for (i in 12...17) i], 6, true);      
   }
 
   function pacing() {
@@ -34,15 +35,27 @@ class Leopard extends Enemy {
     }
   }
 
+  function running() {
+    animation.play("running");
+  }
+
   function movingLeft(FaceLeft:Bool = true) {
-    var walkingDistance:Int = 30;
+    var walkingDistance:Int = 200;
 		velocity.x = FaceLeft ? -walkingDistance: walkingDistance;
 		facing = FaceLeft ? FlxObject.LEFT : FlxObject.RIGHT;
+  }
+
+	override public function kill() {
+    _enemyDying = true;
+		dieSlowly();
   }
 
   override public function update(Elapsed:Float):Void {
     super.update(Elapsed);
     _seconds += Elapsed;
-    pacing();
+    _enemyDying ? {
+      animation.play("dying");
+      velocity.x = 0;
+    } : pacing();
   }
 }
