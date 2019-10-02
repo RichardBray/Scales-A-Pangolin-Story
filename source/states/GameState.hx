b329f520b84ba8ca12f217e7cc73b3070ad02683
Richard Bray <richardbray89@gmail.com>
2019-10-02 08:47:13 +0100

    Tried to fix mous issue for prod build

--- source/states/GameState.hx
+++ source/states/GameState.hx
@@ -8,7 +8,7 @@ class GameState extends FlxState {
   override public function create() {
 		FlxG.autoPause = false;
 		#if !debug
-		FlxG.mouse.visible = false; // Hide the mouse cursor
+		FlxG.mouse.enabled = false; // Hide the mouse cursor
 		#end
 		FlxG.cameras.fade(FlxColor.BLACK, 0.5, true); // Level fades in    
   }
