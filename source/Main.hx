5cd59c8dfb7da35a127680b50d13fca8a338c950
Richard Bray <richardbray89@gmail.com>
2019-10-02 08:47:02 +0100

    Add if statement for main game

--- source/Main.hx
+++ source/Main.hx
@@ -6,7 +6,10 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		// addChild(new FlxGame(1920, 1080, MainMenu.HLScreen, 1, 60, 60, true));
+		#if !debug
+		addChild(new FlxGame(1920, 1080, MainMenu.HLScreen, 1, 60, 60, true));
+		#else 
 		addChild(new FlxGame(1920, 1080, levels.LevelFour, 1, 60, 60, true));
+		#end
 	}
 }
